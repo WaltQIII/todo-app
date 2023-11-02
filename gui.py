fc0947a854724b8c7efa73cd87e1c11aaee6f791
Key Quarles <wquarles25@gmail.com>
2023-11-01 23:07:09 -0700

    Implement the Complete and Exit buttons

--- gui.py
+++ gui.py
@@ -10,12 +10,15 @@ add_button = sg.Button("Add")
 list_box = sg.Listbox(values=functions.get_todos(todo_filepath), key='todos',
                       enable_events=True, size=(45, 10))
 edit_button = sg.Button("Edit")
+complete_button = sg.Button("Complete")
+exit_button = sg.Button("Exit")
 
 # Window layout
 layout = [
     [label],
     [input_box, add_button],
-    [list_box, edit_button]
+    [list_box, edit_button, complete_button],
+    [exit_button]
 ]
 
 window = sg.Window('My To-Do App', layout, font=('Helvetica', 20))
@@ -50,6 +53,15 @@ while True:
                     sg.popup_error(f"'{todo_to_edit}' not found in todos list")
             else:
                 sg.popup_error("Please select a todo to edit")
+        case "Complete":
+            todo_to_complete = values['todos'][0]
+            todos = functions.get_todos(todo_filepath)
+            todos.remove(todo_to_complete)
+            functions.write_todos(todo_filepath, todos)
+            window['todos'].update(values=todos)
+            window['todo'].update(value='')
+        case "Exit":
+            break
         case 'todos':
             # Update the input box to show the selected todo
             window['todo'].update(value=values['todos'][0])
